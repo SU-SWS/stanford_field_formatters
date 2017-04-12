5a838945048e5d3c16efe7eb455ae9bb21351221
Mike Decker <pookmish@gmail.com>
2017-04-12 10:43:40 -0700

    fixed styles when using chosen

--- js/stanford_field_formatters.js
+++ js/stanford_field_formatters.js
@@ -7,12 +7,7 @@
   Drupal.behaviors.stanford_field_formatters = {
     attach: function (context, settings) {
       $('.stanford-fontawesome-icon', context).find('option').each(function () {
-        if (!$(this).hasClass('icon-added')) {
-          var icon = $('<i>', {
-            class: 'fa fa-' + $(this).attr('value')
-          });
-          $(this).addClass('icon-added').prepend(icon);
-        }
+        $(this).addClass('fa').addClass('fa-' + $(this).attr('value'));
       });
     }
   }
